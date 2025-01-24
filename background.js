8f413dc495cbc6a71fed3f0b5e3a40c2493b54d5
showcloasetabbutton <zhihaushiu+showclosetabbutton@gmail.com>
2025-01-24 15:42:15 +0800

    remove files and remove margin from index file

--- background.js
+++
@@ -1,9 +0,0 @@
-// listen message from content-script 
-browser.runtime.onMessage.addListener((message, sender) => {
-//  console.log('backgound script receives message');
-  if (message.action === "closeTab") {
-    // close current tab
-    browser.tabs.remove(sender.tab.id);
-  }
-});
-
